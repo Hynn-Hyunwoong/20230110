d3cc9abc9dfb88d7249bdb9ffb159c52f483c769
Hynn <choihwoong@me.com>
2023-01-10 16:04:52 +0900

    change directory

--- Front/server.js
+++ Front/server.js
@@ -2,8 +2,10 @@ require("dotenv").config();
 const express = require('express');
 const app = express();
 const nunjucks = require('nunjucks');
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000
+
+// console.log(process.env.PORT)
 
 app.listen(PORT, () =>{
-    console.log('This is front server start only port on ${PORT}')
+    console.log(`This is front server start only port on ${PORT}`)
 })
